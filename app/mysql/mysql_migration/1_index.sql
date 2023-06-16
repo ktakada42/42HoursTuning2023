1462c445856f28cec2a4ec4965ef66e185d15074
ktakada42 <k_takada@dreamarts.co.jp>
2023-06-17 00:38:03 +0900

    feat: とりあえず適当にインデックス付与

--- app/mysql/mysql_migration/1_index.sql
+++ app/mysql/mysql_migration/1_index.sql
@@ -1,1 +1,5 @@
-CREATE INDEX idx_user_id ON match_group_member(user_id)
+CREATE INDEX idx_user_id ON match_group_member(user_id);
+CREATE INDEX idx_user_id_and_belong ON department_role_member(user_id, belong);
+CREATE INDEX idx_role_id_and_belong ON department_role_member(role_id, belong);
+CREATE INDEX idx_entry_date_and_kana ON user(entry_date, kana);
+CREATE INDEX idx_mail_and_password ON user(mail, password);
