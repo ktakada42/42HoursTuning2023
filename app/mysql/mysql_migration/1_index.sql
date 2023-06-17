5e989d2eb5c3ee0f0e580e2eebe286105c232c17
ktakada42 <k_takada@dreamarts.co.jp>
2023-06-17 09:49:43 +0900

    feat: userテーブルにoffice_idとuser_idの複合インデックスを付与

--- app/mysql/mysql_migration/1_index.sql
+++ app/mysql/mysql_migration/1_index.sql
@@ -3,3 +3,4 @@ CREATE INDEX idx_user_id_and_belong ON department_role_member(user_id, belong);
 CREATE INDEX idx_role_id_and_belong ON department_role_member(role_id, belong);
 CREATE INDEX idx_entry_date_and_kana ON user(entry_date, kana);
 CREATE INDEX idx_mail_and_password ON user(mail, password);
+CREATE INDEX idx_office_id_and_user_id ON user(office_id, user_id);
